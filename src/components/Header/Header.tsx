333d4bcb5e26484d124de9f2682cc196721d7a4b
Travline <poluxmorales1@gmail.com>
2026-04-08 16:42:10 -0500

    Initializing header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import styles from "./Header.module.css"
+// Usando CSS Modules
+
+export const Header = () => {
+  return (
+    <header
+      className={styles.header}
+    >
+      El header p
+    </header>
+  )
+}
