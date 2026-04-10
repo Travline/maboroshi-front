f465eb447fac1bde5044e2a055a8d0d30fcef5bc
G3rome <ismaelgelo@hotmail.com>
2026-04-09 19:57:20 -0500

    implementacion del footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css'
 import { Header } from './components/Header/Header'
-
+import Footer from './components/Footer/Footer'
 function App() {
 
   return (
     <>
       <Header/>
+      <main/>
       <h1>oal</h1>
+      <main/>
+      <Footer/>
     </>
   )
-}
+} 
 
 export default App
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                <div className="footer-left">
+                    <a href="#">CONTACTO</a>
+                    <a href="#">SOCIAL MEDIA</a>
+                </div>
+                <div className="footer-right">
+                    © 2026 Maboroshi Records. All Rights Reserved
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
