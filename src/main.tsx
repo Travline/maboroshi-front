83d146f8c75499ea117453b25f429d58cad65102
Travline <poluxmorales1@gmail.com>
2026-04-15 16:32:55 -0500

    fixing routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Header } from './components/Header/Header.tsx'
 
 import { router } from './routes.tsx'
 import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Header />
     <RouterProvider router={router}/>
   </StrictMode>,
 )
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,16 @@
-import { createBrowserRouter} from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom'
+import { MainLayout } from './layouts/MainLayout.tsx'
 import App from './App.tsx'
 import { Oal } from './pages/Oal.tsx'
 
 export const router = createBrowserRouter([
-  {path:"/", element: <App/>},
-  {path:"/oal", element: <Oal />},
-  {path:"*", element: <Oal />}, //Este para el 404 not found
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/oal", element: <Oal /> },
+      { path: "*", element: <Oal /> }, //Este para el 404 not found
+    ]
+  }
 ])
