ae4f0ccb98c7e39d235de2b5b64ec67597312f63
Angelo Quispe <angeloqui12345@gmail.com>
2026-04-18 15:44:54 -0500

    mejora de banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import './App.css'
-<<<<<<< HEAD
 import { Header } from './components/Header/Header'
 import { Banner } from './components/Banner/Banner'
-=======
 import Footer from './components/Footer/Footer'
->>>>>>> origin/main
 
 function App() {
-
   return (
     <>
-<<<<<<< HEAD
-      <Header/>
-      <Banner/>
-=======
+      <Header />
+      <Banner title="Nombre del disco"
+        artist="Autor"
+        price={200}
+        image="https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4"
+        background="https://images.unsplash.com/photo-1497032205916-ac775f0649ae" />
       <main>
         <h1>Contenido de la ruta "/"</h1>
       </main>
       <Footer />
->>>>>>> origin/main
     </>
   )
 }
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,19 +1,35 @@
 import styles from "./Banner.module.css";
 
-export const Banner = () => {
+type BannerProps = {
+  title: string;
+  artist: string;
+  price: number;
+  image: string;
+  background: string;
+};
+
+export const Banner = ({
+  title,
+  artist,
+  price,
+  image,
+  background,
+}: BannerProps) => {
   return (
-    <section className={styles.banner}>
-      <div className={styles.card}>
+    <section
+      className={styles.banner}
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div className={styles.overlay} />
+
+      <div className={styles.content}>
         <h2>MABOROSHI ARCHIVES</h2>
 
-        <img
-          src="https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4"
-          alt="album"
-        />
+        <img src={image} alt={title} />
 
-        <h3>Nombre del disco</h3>
-        <p>Autor</p>
-        <span>$200.00</span>
+        <h3>{title}</h3>
+        <p>{artist}</p>
+        <span>S/.{price}</span>
 
         <button>+</button>
       </div>
