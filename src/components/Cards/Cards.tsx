471d9d65bc96e03e5c800da82583175ab48e580d
Gabovoid <dar.angel.g1206@gmail.com>
2026-04-27 21:27:39 -0500

    primer avance de slider de cards

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -10,9 +10,11 @@ export const Cards = ({ product }: props) => {
         <div className={styles.Card}>
             <a href="">
                 <img className={styles.ImgCard} src={product.image} alt="Loading..." />
-                <h3 className={styles.NameCard}>{product.name}</h3>
-                <h3 className={styles.ArtistCard}>{product.artist}</h3>
-                <h3 className={styles.PriceCard}>S/.{product.price.toFixed(2)}</h3>
+                <div className={styles.InfoCard}>
+                    <h3>{product.name}</h3>
+                    <h2>{product.artist}</h2>
+                    <h3 className={styles.PriceCard}>S/.{product.price.toFixed(2)}</h3>
+                </div>
             </a>
         </div>
     )
