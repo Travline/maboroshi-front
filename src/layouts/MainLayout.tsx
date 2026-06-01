7390eb87f10acc2bc4b7fb9d810fbdfc44ac8da0
Franco <bounszz@hotmail.com>
2026-06-01 17:51:12 -0500

    cmerge de mi rama al main, la funcionalidad del modal del toolbar ya no esta funcional

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { Header } from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
 import { Outlet } from "react-router-dom";
+import { ToolbarModal } from "../components/ToolbarModal/ToolbarModal";
 import Footer from '../components/Footer/Footer'
 
 export const MainLayout = () => {
