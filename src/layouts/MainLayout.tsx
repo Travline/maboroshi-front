8742be90cccb4f80514caa34a1c6a602135ffd1b
Franco <bounszz@hotmail.com>
2026-06-01 05:11:20 -0500

    Agregando header, cambios realizados en index.css para normalizar html

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,13 @@
 import { Header } from "../components/Header/Header";
 import { Outlet } from "react-router-dom";
+import Footer from '../components/Footer/Footer'
 
 export const MainLayout = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   )
 }
