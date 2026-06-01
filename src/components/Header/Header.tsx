1a3c1967ac6261ef9f08d991be4066b178b66d9f
Travline <poluxmorales1@gmail.com>
2026-06-01 13:02:22 -0500

    adding simple header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,14 @@
+import { useToolbarModalStore } from "../../hooks/ToolbarModalStore"
 import styles from "./Header.module.css"
-import { Link } from "react-router-dom"
-// Usando CSS Modules
 
 export const Header = () => {
+  const { open, close, isOpened } = useToolbarModalStore()
+  
   return (
     <header>
-      <nav>
-        <Link to={"#"}>GENEROS</Link>
-        <Link to={"#"}>VINYL</Link>
-        <Link to={"/"} className={styles.logo}>
-          <img src="/assets/logo.png" alt="Logo Maboroshi" />
-        </Link>
-        <Link to={"#"}>CD</Link>
-        <Link to={"#"}>PRE - VENTA</Link>
-      </nav>
+        <div onClick={() => {isOpened ? close() : open()}} className={styles.logo}>
+          <img src="/assets/smiley 1.png" alt="Logo Maboroshi" />
+        </div>
     </header>
   )
 }
