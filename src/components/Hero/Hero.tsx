d6099942471857abbf9f59b573b72392f1cca838
Angelo Quispe <angeloqui12345@gmail.com>
2026-06-05 02:16:28 -0500

    avance del homepage

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,1 +1,43 @@
-// oal
+import styles from "./Hero.module.css";
+import { useToolbarModalStore } from "../../hooks/ToolbarModalStore";
+
+export const Hero = () => {
+
+    const { open } = useToolbarModalStore();
+
+    const currentAlbum = {
+        name: "OCTANE",
+        image: "https://i.scdn.co/image/ab67616d0000b27325c28f3c9fbdbab1a88dd619"
+    };
+
+    return (
+        <section className={styles.hero}>
+            <img
+                src="/assets/Captura de pantalla 2026-05-26 231402.png"
+                className={styles.logo}
+                alt="Maboroshi"
+            />
+
+            <h1 className={styles.heroTitle}>
+                MABOROSHI RANDOM
+            </h1>
+
+            <img
+                className={styles.albumImage}
+                src={currentAlbum.image}
+            />
+
+            <p className={styles.albumTitle}>
+                {currentAlbum.name}
+            </p>
+
+            <button
+                className={styles.exploreButton}
+                onClick={open}
+            >
+                EXPLORA
+            </button>
+
+        </section>
+    );
+};
--- src/routes.tsx
+++ src/routes.tsx
@@ -5,17 +5,22 @@ import { Oal } from './pages/Oal.tsx'
 import { AuthModal } from './components/AuthModal/AuthModal.tsx'
 import { NotFound } from './components/404 ERROR/404 ERROR.tsx'
 import { LibroReclamaciones } from './pages/LibroReclamaciones.tsx'
+import { HomePage } from './pages/HomePage'
 
 export const router = createBrowserRouter([
   {
     path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/",
     element: <MainLayout />,
     children: [
-      { path: "/", element: <App /> },
+      { path: "/store", element: <App /> },
       { path: "/login", element: <AuthModal /> },
       { path: "/oal", element: <Oal /> },
       { path: "/libro-reclamaciones", element: <LibroReclamaciones /> },
       { path: "*", element: <NotFound /> },
     ]
   }
-])
+]);
