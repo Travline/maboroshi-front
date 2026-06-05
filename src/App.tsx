17efec9d1b2cf47afe934565c66e26f161b6f8cd
Angelo Quispe <angeloqui12345@gmail.com>
2026-06-05 13:28:27 -0500

    solucion de error, conexion de  sliders con backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
 import { Slider } from './components/Sliders/Sliders'
 import { useState, useEffect } from 'react'
-import { getPicks, getVinylCollections } from './mocks/cards'
 import type { Product } from "./models/Card"
 import { Collections } from './components/Collections/Collections'
 import { ArtistCard } from './components/Artist/Artist'
@@ -11,26 +10,17 @@ import { getProducts } from "./api/products";
 
 
 function App() {
-  const [picks, setPicks] = useState<Product[]>([]);
-  const [vinylCollections, setVinylCollections] = useState<Product[]>([]);
   const [artist, setArtist] = useState<Artist[]>([]);
   const [products, setProducts] = useState<Product[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loadData = async () => {
-      const picksData = await getPicks();
-
-      const vinylData = await getVinylCollections();
       const artistsData = await getArtist();
-
-      setPicks(picksData);
-      setVinylCollections(vinylData);
       setArtist(artistsData);
     };
 
     loadData();
-
     const loadProducts = async () => {
       try {
         const data = await getProducts();
@@ -65,7 +55,7 @@ function App() {
         <Collections />
 
         <div>
-          <Slider title="MABOROSHI COLLECTIONS" products={picks} />
+          <Slider title="MABOROSHI COLLECTIONS" products={products} />
 
         </div>
         <div>
