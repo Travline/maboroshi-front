24bf378c8b8ae2f47f7c26a9cb49101700ff6ae7
Angelo Quispe <angeloqui12345@gmail.com>
2026-06-05 13:09:55 -0500

    avance de coneccion con el backend

--- src/App.tsx
+++ src/App.tsx
@@ -5,44 +5,67 @@ import { getPicks, getVinylCollections } from './mocks/cards'
 import type { Product } from "./models/Card"
 import { Collections } from './components/Collections/Collections'
 import { ArtistCard } from './components/Artist/Artist'
-import {getArtist } from './mocks/artist'
+import { getArtist } from './mocks/artist'
 import type { Artist } from './models/Artist'
+import { getProducts } from "./api/products";
+
 
 function App() {
   const [picks, setPicks] = useState<Product[]>([]);
   const [vinylCollections, setVinylCollections] = useState<Product[]>([]);
   const [artist, setArtist] = useState<Artist[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loadData = async () => {
       const picksData = await getPicks();
 
-      const vinylData = await getVinylCollections();  
+      const vinylData = await getVinylCollections();
       const artistsData = await getArtist();
-
 
       setPicks(picksData);
       setVinylCollections(vinylData);
       setArtist(artistsData);
-    }
+    };
 
     loadData();
 
+    const loadProducts = async () => {
+      try {
+        const data = await getProducts();
+        setProducts(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadProducts();
+
   }, []);
 
+
+  if (loading) {
+    return <h1>Cargando productos...</h1>;
+  }
   return (
     <>
       <main>
         <div>
+          <Slider
+            title="MABOROSHI COLLECTIONS"
+            products={products}
+          />
+
+          <Slider title="MABOROSHI PICKS" products={products} />
+        </div>
+
+        <Collections />
+
+        <div>
           <Slider title="MABOROSHI COLLECTIONS" products={picks} />
-
-        <Slider title="MABOROSHI PICKS" products={vinylCollections} />
-      </div>
-
-      <Collections />
-
-      <div>
-        <Slider title="MABOROSHI COLLECTIONS" products={picks} />
 
         </div>
         <div>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,37 @@
 import { Hero } from "../components/Hero/Hero";
 import { ToolbarModal } from "../components/ToolbarModal/ToolbarModal";
 
+import { useEffect, useState } from "react";
+import { getProducts } from "../api/products";
+import type { Product } from "../models/Card";
+
 export const HomePage = () => {
+
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      try {
+        const data = await getProducts();
+        setProducts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    loadProducts();
+  }, []);
+
+  if (products.length === 0) {
+    return <h1>Cargando...</h1>;
+  }
+
+  const randomAlbum =
+    products[Math.floor(Math.random() * products.length)];
   return (
     <>
       <ToolbarModal />
-      <Hero />
+      <Hero album={randomAlbum} />
     </>
   );
 };
