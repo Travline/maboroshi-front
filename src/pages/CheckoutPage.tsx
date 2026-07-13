d213cd3273c0325a53e5741426fed018801e63a9
Travline <poluxmorales1@gmail.com>
2026-07-13 14:06:44 -0500

    lol

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,15 +1,20 @@
 import { CardPayment, initMercadoPago } from "@mercadopago/sdk-react";
 import { ENV } from "../api/environment";
+import styles from "./CheckoutPage.module.css"
 
 export const CheckoutPage = () => {
   initMercadoPago(ENV.VITE_MP_PUBLIC_KEY, {
     locale: "es-PE"
   })
 
-  return <CardPayment
-    initialization={{ amount: 1.00 }}
-    onSubmit={async (param) => {
-      console.log(param);
-    }}
-  />
+  return (
+    <div className={styles.checkoutContainer}>
+      <CardPayment
+        initialization={{ amount: 1.00 }}
+        onSubmit={async (param) => {
+          console.log(param);
+        }}
+      />
+    </div>
+  )
 }
